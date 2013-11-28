86be877079b0d5bad2787583385e3e9a3dd93ac1
omeriko9 <omeriko9@gmail.com>
2013-11-28 20:44:49 +0200

    added message to FB button

--- scripts/global.js
+++ scripts/global.js
@@ -1,10 +1,18 @@
-﻿(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=143979718949629";
-    fjs.parentNode.insertBefore(js, fjs);
-} (document, 'script', 'facebook-jssdk'));
+﻿//(function (d, s, id) {
+//    var js, fjs = d.getElementsByTagName(s)[0];
+//    if (d.getElementById(id)) return;
+//    js = d.createElement(s); js.id = id;
+//    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=143979718949629";
+//    fjs.parentNode.insertBefore(js, fjs);
+//} (document, 'script', 'facebook-jssdk'));
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=143979718949629";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 (function () {
     var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
