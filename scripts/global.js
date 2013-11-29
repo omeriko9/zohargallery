cded9f77e6ae7ddd33f40ac9de700349df243073
ronyshohat <ronyshohat@Ronys-MacBook-Pro.local>
2013-11-29 18:37:26 +0200

    mmmmmmmm

--- scripts/global.js
+++ scripts/global.js
@@ -31,3 +31,57 @@
   ga('create', 'UA-46089701-1', 'omeriko9.com');
   ga('send', 'pageview');
 
+/*
+$(window).scroll(function(e) {
+    // Get the position of the location where the scroller starts.
+    var scroller_anchor = $(".scroller_anchor").offset().top;
+     
+    // Check if the user has scrolled and the current position is after the scroller start location and if its not already fixed at the top 
+    if ($(this).scrollTop() >= scroller_anchor && $('.scroller').css('position') != 'fixed') 
+    {    // Change the CSS of the scroller to hilight it and fix it at the top of the screen.
+      
+       $('ul').css({
+           
+            'position': 'fixed',
+    'margin':'0 auto',
+    'vertical-align':'middle',
+    'width':'100%',
+    
+    'font-size': '16px',
+    'font-family': 'Tahoma, Geneva, sans-serif',
+    
+    'text-align': 'center',
+    /*text-shadow: 1px 1px 2px #333333;*/
+    'background-color': 'rgba(50, 20, 50, .33)',
+    'border-radius': '0px',
+    'box-shadow': '0px 6px 15px #CCC',
+    
+            
+        });
+       
+       $('#logo').css({
+           
+            'height': '0',
+        });
+           $('#menu').css({
+           
+            'background-color': 'rgba(255, 255, 255, 0)',
+            'box-shadow': '0 0 0 #CCC',
+        });
+        // Changing the height of the scroller anchor to that of scroller so that there is no change in the overall height of the page.
+       
+    } 
+    else if ($(this).scrollTop() < scroller_anchor && $('.scroller').css('position') != 'relative') 
+    {    // If the user has scrolled back to the location above the scroller anchor place it back into the content.
+         
+        // Change the height of the scroller anchor to 0 and now we will be adding the scroller back to the content.
+       // $('.scroller_anchor').css('height', '0px');
+         
+        // Change the CSS and put it back to its original position.
+        //$('.scroller').css({
+        //    'background': '#FFF',
+        //    'border': '1px solid #CCC',
+        //    'position': 'relative'
+        //});
+    }
+});*/
