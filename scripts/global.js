1f605656255739e0366d404f882b8b73a84168ca
ronyshohat <ronyshohat@Ronys-MacBook-Pro.local>
2013-12-01 17:35:45 +0200

    fade

--- scripts/global.js
+++ scripts/global.js
@@ -35,10 +35,13 @@
          {
          var the_height=document.getElementById('iframe').contentWindow.document.body.scrollHeight;
          document.getElementById('iframe').style.height= the_height+"px";
+           $('#iframe').hide();
+           $('#iframe').fadeIn(700);
          }
 
    function clearHeight()
          {
-       
-         document.getElementById('iframe').style.height= "400px";
+     
+         document.getElementById('iframe').style.height= "0";
+              
          }
