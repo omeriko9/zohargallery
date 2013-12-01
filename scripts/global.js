56ef686dbfe1a2e8911e8318ca145d0fabf7d2a2
ronyshohat <ronyshohat@Ronys-MacBook-Pro.local>
2013-12-01 13:32:51 +0200

    new icon

--- scripts/global.js
+++ scripts/global.js
@@ -35,6 +35,6 @@
          {
          var the_height=document.getElementById('iframe').contentWindow.document.body.scrollHeight;
          //alert(the_height);
-         document.getElementById('iframe').height= the_height+"px";
+         document.getElementById('iframe').style.height= the_height+"px";
          }
 
