4c1e167fb127afd7c426659dd1fab1f6e99d0d1f
omeriko9 <omeriko9@gmail.com>
2015-03-03 01:26:27 +0200

    added purchasing of painting and the paintings

--- scripts/index.js
+++ scripts/index.js
@@ -21,3 +21,31 @@ else
 function prepFancyBox() {
     $('#iframe').contents().find(".gallery a").fancybox();
 }
+
+$(document).ready(function () {
+    checkDOMChange();
+    $('#menu a[id=order]').animate({ color: '#FF0000' }, 'slow');
+
+    function animate() {
+        $('#menu a[id=order]').animate({ color: '#FF4136' }, 1500, function () {
+            $('#menu a[id=order]').animate({ color: '#FFDC00' }, 1500, function () {
+                animate();
+            });
+        });
+    }
+    animate();
+});
+
+function checkDOMChange() {
+    if ($('#facebook_span').children('span').first().length) {
+        setTimeout(function () {
+            $('#facebook_span').children('span').first().css('height', '30px').css('margin-right', '20px');
+        }, 400);
+        return;
+    }
+    // check for any new element being inserted here,
+    // or a particular node being modified
+
+    // call the function again after 100 milliseconds
+    setTimeout(checkDOMChange, 100);
+}
